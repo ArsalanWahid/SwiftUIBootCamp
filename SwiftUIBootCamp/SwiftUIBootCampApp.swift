2ef6744cb9280d9faa53f9d9dbcc05b5f2f2522e
Arsalan Wahid Asghar <arsalanwahid1993@gmail.com>
2024-09-22 21:15:10 +0500

    Initial Commit

---
+++ SwiftUIBootCamp/SwiftUIBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootCampApp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Arsalan Wahid Asghar on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
