8d8cce1d0f6bede4ff3ab16882608773f859a403
Arsalan Wahid Asghar <arsalanwahid1993@gmail.com>
2024-09-23 21:14:13 +0500

    [Clean] Downgraded app to iOS 17 due to Swift UI preview bug

---
+++ SwiftUIBootCamp/TextBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  TextBootCamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Arsalan Wahid Asghar on 9/22/24.
+//
+
+import SwiftUI
+
+struct TextBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextBootCamp()
+}
