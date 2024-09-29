6d03664f4670a44f165a79ff299f5585a2cfb02d
Arsalan Wahid Asghar <arsalanwahid1993@gmail.com>
2024-09-29 17:50:41 +0500

    [Feature] Navigation example

--- SwiftUIBootCamp/Navigation.swift
+++ SwiftUIBootCamp/Navigation.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct Navigation: View {
+    
+    @State var fruits: [String] = ["Apple","Red grapes","banana"]
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section(header: Text("Fruits")) {
+                    ForEach(fruits, id: \.self) { fruit in
+                        Text(fruit.capitalized)
+                    }.onDelete(perform: delete)
+                    //Rare instance where direct function is being taken
+                }
+            }.navigationTitle("Shopping List")
+                .navigationBarItems(leading: EditButton())
+        }
+    }
+    
+    func delete (indexSet: IndexSet) {
+        fruits.remove(atOffsets: indexSet)
     }
 }
 
