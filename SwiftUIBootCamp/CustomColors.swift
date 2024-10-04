282c3dc00517995ca9198f6c6d776b3816098e44
Arsalan Wahid Asghar <arsalanwahid1993@gmail.com>
2024-10-04 20:28:42 +0500

    [Feature] Revised UI for concept testing

--- SwiftUIBootCamp/CustomColors.swift
+++ SwiftUIBootCamp/CustomColors.swift
@@ -13,14 +13,22 @@ import SwiftUI
 
 struct CustomColors: View {
     var body: some View {
-        
         ZStack(alignment: .center){
             RoundedRectangle(cornerRadius: 25.0).fill(Color("CyberpunkBackground"))
-            VStack(spacing: -200) {
-                Circle().trim(from: 0.5, to: 1.0).stroke(lineWidth: 3).fill(Color("border")).frame(width: 350)
-                Text("cyberpunk".uppercased()).foregroundColor(Color("Cyberpunk")).font(.largeTitle).fontWeight(.bold).italic().shadow(color: Color("cyberpunk").opacity(1) , radius: 10, x: 20, y: 20)
-            }
+            VStack(spacing: 0) {
+                ZStack {
+                    Circle().fill(.red).padding(.horizontal, 10).frame(width: 300).background(
+                        Circle().trim(from: 0.5, to: 1).stroke(.green, lineWidth: 10).fill(Color("Cyberpunk")).frame(width: 301,height: 301)
+                    ).background(
+                        Circle().trim(from: 0.5, to: 1).stroke(.blue, lineWidth: 10).fill(.pink).frame(width: 301,height: 301).rotationEffect(.degrees(180))
+                    )
 
+                    Text("cyberpunk".uppercased()).foregroundColor(Color("Cyberpunk")).font(.largeTitle).fontWeight(.bold).italic().shadow(color: Color("cyberpunk").opacity(1) , radius: 10, x: 20, y: 20)
+                }
+                   
+                
+            }
+            
         }
     }
 }
